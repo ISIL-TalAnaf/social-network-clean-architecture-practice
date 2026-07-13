5d3cd9dacb928a0102311e71efc70279a7be1685
ISIL-TalAnaf <talan@iscar.co.il>
2026-07-13 06:27:25 +0300

    initial commit

---
+++ AnafSocial/AnafSocial.Domain/Entities/Friend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnafSocial.Domain.Entities;
+
+internal class Friend
+    {
+    [Key]
+    public Guid FollowingId { get; set; }
+    [Key]
+    public Guid FollowerId { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}
+
--- AnafSocial/AnafSocial.Domain/Entities/User.cs
+++ AnafSocial/AnafSocial.Domain/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace AnafSocial.Domain.Entities;
 
 public class User
 {
+    [Key]
     public Guid Id { get; set; }
-
+    [Required]
     public string UserName { get; set; } = string.Empty;
+    [Required]
     public string Name { get; set; } = string.Empty;
+    [Required]
     public string Email { get; set; } = string.Empty;
 
     private string password = string.Empty;
